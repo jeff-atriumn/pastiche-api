26338508044c5fcbd1ad9296d45b300b358c86ff
Jeff Rogers <jrojers@gmail.com>
2021-07-19 14:39:30 -0500

    updating updates to portraits

--- update.js
+++ update.js
@@ -7,14 +7,19 @@ export const main = handler(async (event, context) => {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be updated
     Key: {
-        userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
-        imageId: event.pathParameters.id, // The id of the note from the path
+      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+      portraitId: event.pathParameters.id, // The id of the portrait from the path
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET image = :image",
+    UpdateExpression:
+      "SET portrait = :portrait, overlayId = :overlayId, latitude = :latitude, longitude = :longitude, altitude = :altitude",
     ExpressionAttributeValues: {
-      ":image": data.image || null
+      ":portrait": data.image || null,
+      ":overlayId": data.overlayId || null,
+      ":latitude": data.lat || null,
+      ":longitude": data.long || null,
+      ":altitude": data.alt || null,
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
     // where ALL_NEW returns all attributes of the item after the update; you
